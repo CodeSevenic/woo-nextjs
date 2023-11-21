9934375499306aac056dc232df9aaa4e6bb130d2
CodeSevenic <sevenicbookings@gmail.com>
2023-11-21 14:23:12 +0200

    React Native Job Finder

---
+++ src/components/footer/index.js
@@ -0,0 +1,9 @@
+﻿const Footer = () => {
+  return (
+    <footer>
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/header/index.js
@@ -0,0 +1,9 @@
+﻿const Header = () => {
+  return (
+    <header>
+      <h1>My Header</h1>
+    </header>
+  );
+};
+
+export default Header;
